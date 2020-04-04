50bded49f89510d471d202fb1097875ded2a4ca6
Alibi Mourad <mourad.alibi@enis.tn>
2020-04-04 14:42:35 +0100

    ValidationError and Updated ErrorDetail Classes

--- src/main/java/exception/FieldValidationError.java
+++ src/main/java/exception/FieldValidationError.java
@@ -1,5 +1,38 @@
+/**
+ * 
+ */
 package exception;
 
+import java.awt.TrayIcon.MessageType;
+
+/**
+ * @author alibi
+ *
+ */
 public class FieldValidationError {
 
+	private String filed;
+	private String message;
+	private MessageType type; // SUCCESS, INFO, WARNING, ERROR
+	
+	public String getFiled() {
+		return filed;
+	}
+	public void setFiled(String filed) {
+		this.filed = filed;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public MessageType getType() {
+		return type;
+	}
+	public void setType(MessageType type) {
+		this.type = type;
+	}
+	
+	
 }
