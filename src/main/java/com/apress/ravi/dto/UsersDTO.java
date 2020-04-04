7d94d52aedc599c7789d2f7a4efb657a179eaa95
Alibi Mourad <mourad.alibi@enis.tn>
2020-04-04 13:52:12 +0100

    fix UserDTO

--- src/main/java/com/apress/ravi/dto/UsersDTO.java
+++ src/main/java/com/apress/ravi/dto/UsersDTO.java
@@ -3,6 +3,7 @@ package com.apress.ravi.dto;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
